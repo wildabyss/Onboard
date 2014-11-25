be5f5e9dce0faf72d48b0e065ef815a0ca8abfc1
wildabyss <wildabyss@gmail.com>
2014-11-24 23:10:48 -0500

    Community prototype functioning

--- www/community.php
+++ www/community.php
@@ -27,16 +27,65 @@
 <!-- main content -->
 <div class="content_column" id="column_middle">
 	<div class="content_column_wrapper" id="column_wrapper_middle">
-		<?php $user->getFriends($friends); ?>
+	
+		<?php 
+			if (isset($_GET['id'])) 
+				$friend = UserQuery::create()->findOneById($_GET['id']); 
+		?>
 		
-		<h1 class="page_title">My Community</h1>
-		<?php foreach ($friends as $friend):?>
-			<div class="friend">
-				<a class="friend_profile_pic"></a>
-				<div style="vertical-align: middle; display:inline-block; height:100px; margin:0;"></div>
-				<a class="friend_name comm_link" href="community.php?id=<?php echo $friend['id']?>"><?php echo $friend['display_name']?></a>
+		<?php if (isset($friend)):?>
+		
+			<div id="profile_section">
+				<a id="profile_pic"></a>
+				<h1 class="profile_name"><?php echo $friend->getDisplayName();?></h1>
+				<a class="user_info">Email: <?php echo $friend->getEmail();?></a>
+				<a class="user_info">Phone: <?php echo $friend->getPhone();?></a>
 			</div>
-		<?php endforeach; ?>
+			
+			<?php $activityLists = $friend->getActivityLists(); ?>
+			<?php foreach ($activityLists as $list):?>
+			
+				<?php if ($list->countActivityListAssociations() == 0):?>
+					<p class="no_activity_msg">This person has no activity.</p>
+				<?php else:?>
+			
+					<ul class="activity_list">
+					
+						<?php $activities = $list->getActiveOrCompletedActivityAssociations(); ?>
+						<?php for ($i=0; $i<count($activities); $i++):?>
+							<?php $actAssoc = $activities[$i];?>
+							
+							<li>
+								<h2><?php echo $actAssoc->getAlias();?></h2>
+								<a class="datetime">Added: <?php echo $actAssoc->getDateAdded()->format('Y-m-d H:i:s');?></a>
+								<a class="interest_tally">
+									<?php echo ActivityListAssociationQuery::countInterestedFriends($user->getPrimaryKey(), $actAssoc->getActivityId());?>
+									interests
+								</a>
+								<p class="post_body">
+									<?php echo $actAssoc->getDescription();?>
+								</p>
+								<div class="expand">...</div>
+							</li>
+						<?php endfor;?>
+					</ul>
+				<?php endif;?>
+			<?php endforeach;?>
+		
+		<?php else:?>
+	
+			<?php $user->getFriends($friends); ?>
+			
+			<h1 class="page_title">My Community</h1>
+			<?php foreach ($friends as $friend):?>
+				<div class="friend">
+					<a class="friend_profile_pic"></a>
+					<div style="vertical-align: middle; display:inline-block; height:100px; margin:0;"></div>
+					<a class="friend_name comm_link" href="community.php?id=<?php echo $friend['id']?>"><?php echo $friend['display_name']?></a>
+				</div>
+			<?php endforeach; ?>
+		
+		<?php endif;?>
 	</div>
 	
 	<?php include "../layout/screen_footer.php"?>

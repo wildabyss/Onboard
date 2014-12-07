5df58a8102e2f170572a6c04ab303df4a9a1407a
wildabyss <wildabyss@gmail.com>
2014-12-07 02:01:03 -0500

    Facebook login and community population

--- database/UserCommunityAssociationQuery.php
+++ database/UserCommunityAssociationQuery.php
@@ -1,6 +1,10 @@
 <?php
 
+use Facebook\FacebookRequest;
+use Facebook\FacebookSession;
 use Base\UserCommunityAssociationQuery as BaseUserCommunityAssociationQuery;
+use Map\UserCommunityAssociationTableMap;
+use Propel\Runtime\Propel;
 
 /**
  * Skeleton subclass for performing query and update operations on the 'user_community_assoc' table.
@@ -14,5 +18,58 @@ use Base\UserCommunityAssociationQuery as BaseUserCommunityAssociationQuery;
  */
 class UserCommunityAssociationQuery extends BaseUserCommunityAssociationQuery
 {
-
-} // UserCommunityAssociationQuery
+	public static function PopulateCommunityFromFacebook(FacebookSession $fbSession, User $curUser){
+		// get Facebook friends and return as a graph object
+		// note that this only returns all the friends in his friendlist who also uses the app
+		$friends = (new FacebookRequest($fbSession, 'GET', '/me/friends'))
+			->execute()->getGraphObject();
+		
+		// convert to array
+		$graphObjArray = $friends->asArray();
+		$conn = Propel::getReadConnection(UserCommunityAssociationTableMap::DATABASE_NAME);
+		foreach ($graphObjArray['data'] as $friend){
+			// find whether there's already an association
+			$sql = <<<EOT
+			select 1 from user_community_assoc uca
+				join user on uca.user_id_left = user.id
+			    where user.fb_id = :friendfbid1
+			    and uca.user_id_right = :curuserid1
+			union
+			select 1 from user_community_assoc uca
+				join user on uca.user_id_right = user.id
+			    where user.fb_id = :friendfbid2
+			    and uca.user_id_left = :curuserid2;
+EOT;
+			$stmt = $conn->prepare($sql);
+			$stmt->execute(
+				array(
+					':friendfbid1'	=> $friend->id,
+					':curuserid1'	=> $curUser->getId(),
+					':friendfbid2'	=> $friend->id,
+					':curuserid2'	=> $curUser->getId()
+				));
+			
+			// assess results
+			$results = $stmt->fetchAll();
+			
+			// if not exist, add association
+			if (count($results)==0){
+				$sql = <<<EOT
+				insert into user_community_assoc
+					(user_id_left, user_id_right)
+				select :curuserid, user.id
+					from user
+					where user.fb_id = :friendfbid;
+EOT;
+				$stmt = $conn->prepare($sql);
+				$stmt->execute(
+					array(
+						':friendfbid'	=> $friend->id,
+						':curuserid'	=> $curUser->getId()
+					));
+				
+				
+			}
+		}
+	}
+}
--- modules/login.php
+++ modules/login.php
@@ -9,7 +9,6 @@ $_PAGE_TITLE = "Sign In";
 $fb_app_id = Utilities::GetFacebookAppId();
 $fb_app_secret = Utilities::GetFacebookAppSecret();
 FacebookSession::setDefaultApplication($fb_app_id, $fb_app_secret);
-
 $fbLoginHelper = new FacebookRedirectLoginHelper('http://192.168.1.126/login');
 
 // if a valid session already exists, redirect to home
@@ -17,23 +16,26 @@ if (isset($_SESSION['fb_session']) && $_SESSION['fb_session']) {
 	header("Location: mylist");
 	die();
 }
+
+
+// get access token and finalize facebook login
+try {
+	$session = $fbLoginHelper->getSessionFromRedirect();
+} catch(FacebookRequestException $ex) {
+	// Facebook login error
+	header("Location: login");
+	die();
+} catch(\Exception $ex) {
+	// generic error
+	header("Location: login");
+	die();
+}
+
 ?>
 
-<?php if (isset($_GET['code'])):?>
+<?php if ($session):?>
 
 	<?php 
-	// get access token and finalize facebook login
-	try {
-		$session = $fbLoginHelper->getSessionFromRedirect();
-	} catch(FacebookRequestException $ex) {
-		// Facebook login error
-		header("Location: login");
-		die();
-	} catch(\Exception $ex) {
-		// generic error
-		header("Location: login");
-		die();
-	}
 	
 	// verify that the user profile is in the database
 	$curUserObj = UserQuery::ValidateFacebookLogin($session);
@@ -44,9 +46,12 @@ if (isset($_SESSION['fb_session']) && $_SESSION['fb_session']) {
 	
 	// try again
 	if ($curUserObj != false) {
-		// begin authenticated session
+		// refresh his community based on Facebook friends
+		UserCommunityAssociationQuery::PopulateCommunityFromFacebook($session, $curUserObj);
+		
+		// finalize authenticated session
 		UserQuery::FinalizeFacebookLogin($session, $curUserObj);
-			
+		
 		// redirect to home page
 		header("Location: mylist");
 		die();

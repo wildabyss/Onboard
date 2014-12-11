070f5763dff4aba57081c30091eb095e1a43d69b
wildabyss <wildabyss@gmail.com>
2014-12-11 08:05:21 -0500

    name on hover

--- modules/layout/interested_friends_view.php
+++ modules/layout/interested_friends_view.php
@@ -3,7 +3,10 @@
 		<ul class="interest_details">
 			<?php foreach ($_INTERESTED_FRIENDS as $friend):?>
 				<li onclick="window.location.href = 'community?id=<?php echo $friend->getId()?>';">
+					<a class="interested_friends_summary" id="interested_friends_hint_<?php echo $_ACT_OBJ_VIEW->getId()?>_<?php echo $friend->getId()?>"><?php echo $friend->getDisplayName()?></a>
 					<a class="feed_profile_pic" 
+						onmouseover="$('#interested_friends_hint_<?php echo $_ACT_OBJ_VIEW->getId()?>_<?php echo $friend->getId()?>').show('fast')" 
+						onmouseout="$('#interested_friends_hint_<?php echo $_ACT_OBJ_VIEW->getId()?>_<?php echo $friend->getId()?>').hide()"
 						style="background-image: url(../profile_pic_cache/<?php echo $friend->getId()?>_small.jpg)"></a>
 				</li>
 			<?php endforeach?>

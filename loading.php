1a46f5d034cccd5c3066ed4ebbf82317e0ff214a
Jimmy <wildabyss@gmail.com>
2015-02-15 00:43:35 -0500

    loading sequence changed

--- loading.php
+++ loading.php
@@ -2,6 +2,11 @@
 
 // This script should be loaded once at the beginning of any front end execution
 
+// PHP error reporting
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+
 // priority loading
 require_once(__DIR__."/vendor/autoload.php");
 require_once(__DIR__."/database/config.php");
@@ -11,11 +16,6 @@ require_once(__DIR__."/modules/classes/utilities.php");
 require_once(__DIR__."/modules/classes/facebook_util.php");
 require_once(__DIR__."/modules/classes/discussion_util.php");
 require_once(__DIR__."/modules/classes/search.php");
-
-// PHP error reporting
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
 
 // FIXME time zone should vary with user location
 date_default_timezone_set('America/Toronto');
